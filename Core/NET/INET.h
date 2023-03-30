5af03facf53963b5cf671a31a6d89774f9fc4ab2
RoITN <mikot8792@gmail.com>
2023-03-31 02:09:35 +0500

    for develop

--- Core/NET/INET.h
+++ Core/NET/INET.h
@@ -1,3 +1,4 @@
+// Nelya commit
 #ifndef NETFRAMEWORK_INET_H
 #define NETFRAMEWORK_INET_H
 
@@ -12,12 +13,64 @@
 #endif
 
 #import <iostream>
+#import "../Core.h"
 
-class Interface {
+class ISocket {
+private:
+    struct sin_addr {
+        core::uint32_t S_addr;
+    };
+
+    typedef struct sock_in {
+        sin_addr            S_un;
+        core::uint16_t  sin_port;
+        core::int32_t sin_family;
+        core::word   sin_zero[8];
+    } pkt_t, *_pkt_t;
+
+    typedef struct sock {
+        core::uint8_t    sa_len;
+        core::uint8_t sa_family;
+        core::word  sa_data[14];
+    } _raw_pkt_t, *__raw_pkt_t;
+
+    typedef struct net_conf {
+        std::vector<SOCKET> socks;
+        std::vector<_pkt_t> headr;
+        std::vector<core::word>
+                      data_buffer;
+
+        std::vector<core::int32_t>
+                     error_buffer;
+    } pkt_conf_t, *_pkt_conf_t;
+
+    _pkt_conf_t              conf;
+
+public:
+    #ifdef WIN64
+        typedef SOCKET            SOCKET;
+        typedef core::int32_t *socklen_t;
+    #else
+        typedef core::uint64_t    SOCKET;
+        typedef socklen_t      socklen_t;
+    #endif // WIN64
+
+    struct user_settings {
+        std::string  ip_addr;
+        core::uint16_t  port;
+
+        std::string l4_proto;
+
+        std::vector<std::string>
+                   log_cache;
+    };
+
+    ISocket(const ISocket&);
 
 
-
-    ~Interface(); //closesocket
+    ~ISocket(); //closesocket
 };
 
 #endif //NETFRAMEWORK_INET_H
+
+// end Nelya commit
--- Core/NET/NET.cpp
+++ Core/NET/NET.cpp
@@ -1,2 +1,17 @@
 #import "INET.h"
 
+ISocket::ISocket(const ISocket& copy_settings) {
+    *this = copy_settings;
+}
+
+ISocket::~ISocket() {
+    for (SOCKET fd : conf->socks) {
+        #ifdef WIN64
+            closesocket(fd);
+        #else
+            close(fd);
+        #endif // WIN64
+    }
+
+    delete(conf);
+}
