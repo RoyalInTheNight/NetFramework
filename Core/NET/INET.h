35da5945fd4362804c79b2c47eef90f70817a0f9
Nelya <milkshameuw@yandex.ru>
2023-03-30 19:01:25 +0500

    .

--- Core/NET/INET.h
+++ Core/NET/INET.h
@@ -1,4 +1,23 @@
 #ifndef NETFRAMEWORK_INET_H
 #define NETFRAMEWORK_INET_H
 
+#ifdef WIN64 // Windows
+#import <WinSock2.h>
+
+#else // Linux
+#import <sys/socket.h>
+#import <arpa/inet.h>
+#import <netinet/in.h>
+
+#endif
+
+#import <iostream>
+
+class Interface {
+
+
+
+    ~Interface(); //closesocket
+};
+
 #endif //NETFRAMEWORK_INET_H
