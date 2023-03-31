ede568336c691cfe6d7a18e10e0a947f621cdca6
MikoG <rointhenight@gmail.com>
2023-04-01 01:12:50 +0500

    comments

--- Core/NET/INET.h
+++ Core/NET/INET.h
@@ -21,18 +21,18 @@ private:
         core::uint32_t S_addr;
     };
 
-    typedef struct sock_in {
+    typedef struct sock_in { // аналог sockaddr_in
         sin_addr            S_un;
         core::uint16_t  sin_port;
         core::int32_t sin_family;
         core::word   sin_zero[8];
-    } pkt_t, *_pkt_t;
+    } pkt_t, *_pkt_t; // замена типов данных
 
-    typedef struct sock {
+    typedef struct sock { // аналог sockaddr
         core::uint8_t    sa_len;
         core::uint8_t sa_family;
         core::word  sa_data[14];
-    } _raw_pkt_t, *__raw_pkt_t;
+    } _raw_pkt_t, *__raw_pkt_t; // замена типов данных
 
     typedef struct net_conf {
         std::vector<SOCKET> socks;
@@ -42,11 +42,11 @@ private:
 
         std::vector<core::int32_t>
                      error_buffer;
-    } pkt_conf_t, *_pkt_conf_t;
+    } pkt_conf_t, *_pkt_conf_t; // структура для надстройки функций начиная с 59 по 63 строку
 
     _pkt_conf_t              conf;
 
-    #ifdef WIN64
+    #ifdef WIN64 // замена типов данных для кросплатформенности
         typedef SOCKET            SOCKET;
         typedef core::int32_t  socklen_t;
     #else
@@ -54,9 +54,9 @@ private:
         typedef socklen_t      socklen_t;
     #endif // WIN64
 
-    std::string &_inet_ntoa(sin_addr);
+    std::string &_inet_ntoa(sin_addr); // перевод ip адреса из числа в строку
 
-    core::empty_type _bind(core::int32_t);
+    core::empty_type _bind();
     core::empty_type _connect();
     core::empty_type _send(std::string &, core::int32_t);
     core::empty_type _recv(core::net::winsock_buffer_type, std::string::size_type, core::int32_t);
@@ -71,13 +71,15 @@ public:
         std::string cache_file;
 
         std::vector<std::string>
-                     log_cache;
-    };
+                     log_cache; // массив, который будет кэшироваться в tmp папке
+    }; // настройки пользователя
 
     user_settings *settings;
 
     ISocket(const ISocket&);
     ISocket(const std::string&, core::uint16_t, const std::string&, const std::string&);
+
+    // основные методы для работы с упрощенными сокетами
 
     core::empty_type ConnectTCP();
     core::empty_type ListenConnect();
