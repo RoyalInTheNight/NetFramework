5151f67ab8647f08e0c4cc78ea02a3392513257b
MikoG <rointhenight@gmail.com>
2023-04-06 16:34:30 +0500

    SPtr fix and tests

--- Core/ISPtr.h
+++ Core/ISPtr.h
@@ -14,18 +14,69 @@ private:
     core::uint64_t size_alloc;
 
 public:
-    ISPtr(const core::uint64_t);
-    ISPtr(const ISPtr<pointer_t>&);
-    ISPtr();
+    ISPtr(ISPtr<pointer_t> &copy) {
+        *this = std::move(copy);
 
-    pointer_t& get_data();
+        std::cout << "Constructor input" << std::endl;
+    }
 
-    ISPtr<pointer_t>& operator+(ISPtr<pointer_t>&);
-    ISPtr<pointer_t>& operator=(ISPtr<pointer_t>&);
-    core::comparison operator==(ISPtr<pointer_t>&);
-    pointer_t operator[](core::uint64_t);
+    ISPtr(core::uint64_t size) : size_alloc(size) {
+        if (this->size_alloc > 1)
+            this->ptr = new pointer_t[size];
 
-    ~ISPtr();
+        else
+            this->ptr = new pointer_t;
+
+        std::cout << "Constructor input" << std::endl;
+    }
+
+    ISPtr() : size_alloc(1) {
+        this->ptr = new pointer_t;
+
+        std::cout << "Constructor input" << std::endl;
+    }
+
+    pointer_t& operator*() {
+        return *this->ptr;
+    }
+
+    pointer_t& operator[](core::uint64_t pos) {
+        if (pos >= this->size_alloc) {
+            std::cerr << "out of range - pos: " << pos << " >= size_alloc: " << this->size_alloc << std::endl;
+            exit(-1);
+        }
+
+        else
+            return this->ptr[pos];
+    }
+
+    void add_memory() {
+        this->ptr = new pointer_t[1];
+        this->size_alloc++;
+    }
+
+    void add_memory(core::uint64_t size) {
+        this->ptr = new pointer_t[size];
+        this->size_alloc += size;
+    }
+
+    inline core::uint64_t size() {
+        return this->size_alloc;
+    }
+
+    inline core::uint64_t bytes_size() {
+        return sizeof(pointer_t) * this->size_alloc;
+    }
+
+    ~ISPtr() {
+        if (size_alloc > 1)
+            delete[] this->ptr;
+
+        else
+            delete this->ptr;
+
+        std::cout << "Destructor input" << std::endl;
+    }
 };
 
 #endif //NETFRAMEWORK_ISPTR_H
---
+++ Core/test_smart_pointers.cpp
@@ -0,0 +1,49 @@
+//
+// Created by MikoG on 06.04.2023.
+//
+
+#include "ISPtr.h"
+#include <thread>
+
+core::int32_t main() {
+    ISPtr<core::int32_t> pointer(3423123);
+
+    uintptr_t begin_address_decimal = (uintptr_t)&pointer[0];
+    uintptr_t end_address_decimal   = (uintptr_t)&pointer[pointer.size() - 1];
+
+    std::cout << "begin address: " << &pointer[0] <<
+                 "\nend address: " << &pointer[pointer.size() - 1] <<
+                 "\nsize pointer: " << pointer.size() << std::endl; // test size output and alloc size
+
+    std::cout << "size according to calculations: " << (end_address_decimal - begin_address_decimal) / sizeof(core::int32_t) << std::endl;
+
+    // add_memory() test
+    pointer.add_memory();
+
+    begin_address_decimal = (uintptr_t)&pointer[0];
+    end_address_decimal   = (uintptr_t)&pointer[pointer.size() - 1];
+
+    std::cout << "begin address: " << &pointer[0] <<
+                 "\nend address: " << &pointer[pointer.size() - 1] <<
+                 "\nsize pointer: " << pointer.size() << std::endl; // test size output and alloc size
+
+    std::cout << "size according to calculations: " << (end_address_decimal - begin_address_decimal) / sizeof(core::int32_t) << std::endl;
+
+    // add_memory(core::uint64_t) test
+    pointer.add_memory(1000000);
+
+    begin_address_decimal = (uintptr_t)&pointer[0];
+    end_address_decimal   = (uintptr_t)&pointer[pointer.size() - 1];
+
+    std::cout << "begin address: " << &pointer[0] <<
+                 "\nend address: " << &pointer[pointer.size() - 1] <<
+                 "\nsize pointer: " << pointer.size() << std::endl; // test size output and alloc size
+
+    std::cout << "size according to calculations: " << (end_address_decimal - begin_address_decimal) / sizeof(core::int32_t) << std::endl;
+
+    // test of writing a number to a memory area
+    for (core::uint64_t i = 0; i < pointer.size() - 1; i++)
+        pointer[i] = i;
+
+    std::cout << "begin index: " << pointer[0] << "\nend index: " << pointer[pointer.size() - 1] << std::endl;
+}
