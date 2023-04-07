edb6cd8bc7057fd62cc99a9d7aa375794434858f
RoyalInTheNight <107641578+RoyalInTheNight@users.noreply.github.com>
2023-04-07 23:45:31 +0500

    Update Core.h

--- Core/Core.h
+++ Core/Core.h
@@ -17,35 +17,55 @@ namespace core {
     typedef long long           int64_t;
     typedef unsigned long long uint64_t;
 
-    typedef bool             comparsion;
-
     // замена типа void, void *
     typedef void                 empty_type;
     typedef core::empty_type *memory_buffer;
 
-    namespace ptr {
-        typedef core::int32_t        size_t;
+    // замена типа, для библиотеки fs
+    namespace fs {
+        typedef core::word *  fs_path;
     }
 
-    // замена типов и инициализация констант для библиотеки net
-    namespace net {
-        typedef core::word *winsock_buffer_type;
+   namespace net {
+        typedef core::word *                winsock_buffer_t;
+        typedef std::string::size_type size_winsock_buffer_t;
 
-        const std::string &tcp = "tcp:pick";
-        const std::string &udp = "udp:pick";
+        const static std::string tcp = "tcp";
+        const static std::string udp = "udp";
 
-        // enumeration удобное представление чисел, чтобы не запутаться и сделать код читаемым
-        enum net_treatment_part {
-            connect = 0, // 0
-            bind, // 1
-            accept // 2
+        enum isocket {
+            connect = 0x0,
+            bind,
+            accept
         };
 
-        #ifndef WIN64 // linux const's
-            const static core::int64_t  INVALID_SOCKET = (-1);
-            const static core::int32_t  SOCKET_ERROR   = (-1);
-        #endif // WIN64
-    }
+        enum linux_errs {
+            init_err = 0x21,
+            socket_err,
+            bind_err,
+            connect_err,
+            listen_err,
+            accept_err,
+            send_err,
+            recv_err,
+            sendto_err,
+            recvfrom_err
+        };
+   }
+
+   namespace http {
+        const static std::string post           = "POST";
+        const static std::string get            = "GET";
+        const static std::string head           = "HEAD";
+        const static std::string put            = "PUT";
+        const static std::string delete_http    = "DELETE";
+        const static std::string options        = "OPTIONS";
+        const static std::string patch          = "PATCH";
+
+        enum http_err {
+            error_hook_ip = 0x31
+        };
+   }
 }
 
 #endif //NETFRAMEWORK_CORE_H
